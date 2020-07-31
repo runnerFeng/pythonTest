71f4f30701473ea96b3eadd2c830ca4ccdf29def
liufeng <liufeng@dae.org>
2020-07-31 12:46:47 +0800

    first practice

---
+++ hello.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+
+# 1.输出单个字符串
+print('hello world')
+
+# 2.输出多个字符串
+print('hello','august')
+# 2.1 \转义
+print('i\'m ok')
+print('i\'m "ok"')
+print('i\'m \"ok\"')
+# 2.2 r''标识''内部的字符无需转义
+print(r'i\'m ok')
+# 2.3 \n换行
+print('the lesson we can \n learn from chuck and all the others...')
+
+# 3.输出整数
+print(300)
+
+# 4.计算
+print(200+100)
+print('200+100=',200+100)
+
+# 5.input()函数输入
+#name = input("please enter your name:")
+#print('hello',name)
+
+# 6.
+
